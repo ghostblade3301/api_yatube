70da8d12a0e33ead8f0d2477f8ae4a51d6bed21d
Pavel Naumenko <ghostblade3301@gmail.com>
2023-04-03 20:55:44 +0300

    review v0

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -1,8 +1,14 @@
 from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 
+# Создаем класс ReadOnlyPermission
 class ReadOnlyPermission(BasePermission):
+    # Переопределяем метод has_object_permission,
+    # который проверяет, имеет ли пользователь право на чтение объекта
     def has_object_permission(self, request, view, obj):
+        # Если метод запроса находится в списке SAFE_METHODS
+        # если они совпадают, то возвращаем True, разрешая соединение,
+        # в противном случае возвращаем False, запрещая соединение
         if request.method in SAFE_METHODS:
             return True
         return request.user == obj.author
